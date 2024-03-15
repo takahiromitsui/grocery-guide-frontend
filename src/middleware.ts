23f836134e78d0a0e58510ba453b69ba8c1f1988
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-15 13:15:53 +0100

    feat: set up router protection

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+	const currentUser = request.cookies.get('connect.sid')?.value;
+
+	if (currentUser && !request.nextUrl.pathname.startsWith('/users')) {
+		return Response.redirect(new URL('/users', request.url));
+	}
+
+	if (!currentUser && !request.nextUrl.pathname.startsWith('/login')) {
+		return Response.redirect(new URL('/login', request.url));
+	}
+}
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
