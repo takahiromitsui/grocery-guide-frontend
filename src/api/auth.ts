1722474c3ad2d6b2ec55796977cc7f9b03ea85ad
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-03-18 14:23:53 +0100

    feat: add logout post

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -9,3 +9,8 @@ export function login(data: any) {
 	const url = '/login';
 	return axios.post(url, data).then(res => res);
 }
+
+export function logout() {
+	const url = '/logout';
+	return axios.post(url).then(res => res);
+}
